a0fa12fa2162b56943563afa2dd878ce6b029801
Lauren Stone <lstone321@gmail.com>
2020-07-19 14:33:34 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -1,10 +1,5 @@
 def bonus
-  epic_tragedy[:montague][:hero][:status] = "dead"
-  epic_tragedy[:capulet][:hero][:status] = "dead"
- 
-puts epic_tragedy
-  
-epic_tragedy{
+  epic_tragedy = {
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
